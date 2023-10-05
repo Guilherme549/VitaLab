69a96f31fe1cfec307b57fd41de807845d6cb21a
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2023-10-05 12:31:01 -0300

    Tela de cadastro e login

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastro/', views.cadastro, name="cadasto")
+    path("cadastro/", views.cadastro, name="cadasto"),
+    path("login/", views.logar, name="login"),
 ]
