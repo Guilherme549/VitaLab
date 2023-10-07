f242e8dc6764716fb80c6fae0ab0aed6e5b051c0
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2023-10-07 00:44:58 -0300

    criando pagina gerenciar pedido

--- exames/views.py
+++ exames/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import TiposExames
+from .models import TiposExames, PedidosExames, SolicitacaoExame
+from datetime import datetime
+from django.contrib import messages
+from django.contrib.messages import constants
 
 
 @login_required  # É uma propriedade que faz chamar esta função somente quando o usuario estiver logado
@@ -18,7 +21,8 @@ def solicitar_exames(request):
 
         preco_total = 0
         for i in solicitacao_exames:
-            preco_total += i.preco
+            if i.disponivel:
+                preco_total += i.preco
 
         return render(
             request,
@@ -28,4 +32,36 @@ def solicitar_exames(request):
                 "solicitacao_exames": solicitacao_exames,
                 "preco_total": preco_total,
             },
-        )   
+        )
+
+
+@login_required
+def fechar_pedido(request):
+    exames_id = request.POST.getlist("exames")
+    solicitacao_exames = TiposExames.objects.filter(id__in=exames_id)
+
+    pedido_exame = PedidosExames(
+        usuario=request.user,
+        data=datetime.now(),
+    )
+
+    pedido_exame.save()
+
+    for exame in solicitacao_exames:
+        solicitacao_exames_temp = SolicitacaoExame(
+            usuario=request.user, exame=exame, status="E"
+        )
+
+        solicitacao_exames_temp.save()
+        pedido_exame.exames.add(solicitacao_exames_temp)
+    pedido_exame.save()
+    messages.add_message(
+        request, constants.SUCCESS, "Pedido de exame realizado com sucesso"
+    )
+    return redirect("/exames/gerenciar_pedidos")
+
+
+def gerenciar_pedidos(request):
+    pedidos_exames = PedidosExames.objects.filter(usuario=request.user)
+
+    return render(request, "gerenciar_pedidos.html", {"pedidos_exames": pedidos_exames})
