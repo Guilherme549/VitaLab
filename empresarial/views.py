479f612f89ce255c0349af07f6db9e2d9c962fcd
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2023-10-14 21:31:21 -0300

    Gerar senha criado

--- empresarial/views.py
+++ empresarial/views.py
@@ -5,6 +5,7 @@ from django.db.models.functions import Concat
 from django.contrib.admin.views.decorators import staff_member_required
 from exames.models import SolicitacaoExame
 from django.http import HttpResponse, FileResponse
+from .utils import gerar_pdf_exames, gerar_senha_aleatoria
 
 
 @staff_member_required
@@ -28,22 +29,42 @@ def gerenciar_clientes(request):
     )
 
 
-@staff_member_required 
+@staff_member_required
 def cliente(request, cliente_id):
     cliente = User.objects.get(id=cliente_id)
     exames = SolicitacaoExame.objects.filter(usuario=cliente)
-    return render(request, 'cliente.html', {'cliente': cliente, 'exames': exames})
+    return render(request, "cliente.html", {"cliente": cliente, "exames": exames})
 
 
-@staff_member_required 
+@staff_member_required
 def exame_cliente(request, exame_id):
     exame = SolicitacaoExame.objects.get(id=exame_id)
-    return render(request, 'exame_cliente.html', {'exame': exame})
+    return render(request, "exame_cliente.html", {"exame": exame})
 
 
-@staff_member_required 
+@staff_member_required
 def proxy_pdf(request, exame_id):
     exame = SolicitacaoExame.objects.get(id=exame_id)
 
     response = exame.resultado.open()
     return FileResponse(response)
+
+
+@staff_member_required
+def gerar_senha(request, exame_id):
+    exame = SolicitacaoExame.objects.get(id=exame_id)
+
+    if exame.senha:
+        # Baixar o documento da senha já existente
+        return FileResponse(
+            gerar_pdf_exames(exame.exame.nome, exame.usuario, exame.senha),
+            filename="token.pdf",
+        )
+
+    senha = gerar_senha_aleatoria(9)
+    exame.senha = senha
+    exame.save()
+    return FileResponse(
+        gerar_pdf_exames(exame.exame.nome, exame.usuario, exame.senha),
+        filename="token.pdf",
+    )
